43c1cc899e50cd6fe7b5815e3e85db5efafbacf6
HackerGhost <ahmedws93@hotmail.com>
2016-04-24 18:46:27 +0200

    Add first Image.

--- game.js
+++ game.js
@@ -3,8 +3,8 @@ var x = 0;
 var y = 0;
 var width = 20;
 var height = 20;
-var scalex = 15 ;
-var scaley = 8 ;
+var scalex = 22 ;
+var scaley = 21;
 var UP = 0 ;
 var DOWN = 3 ;
 var RIGHT = 2 ;
@@ -18,6 +18,7 @@ var pacmanDirection ;
 var aliens = null ;
 var spritesheet = null;
 var assetLoaded = false ;
+var spriteMapIndex = new Map();
 
 
 var grid = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -44,16 +45,33 @@ var grid = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 function initGame() {
 	/// draw maze ;
 	myCanvas = document.getElementById('myCanvas');
+	myCanvas.width = width*scalex ;
+	myCanvas.height = height*scaley;
 	myContext = myCanvas.getContext('2d');
 	drawMaze();
  	spritesheet = new Image();
  	spritesheet.onload = onImageLoad ;
  	spritesheet.src = "sprite.png";
  	console.log("assetLoaded "+assetLoaded);
- 	pacmanX = 1 ;
- 	pacmanY = 8 ;
+ 	populateMap();
+ 	pacmanX = 9 ;
+ 	pacmanY = 1 ;
  	pacmanDirection = DOWN ;
  	startGame();
+}
+
+function populateMap()
+{
+	var pD = [[scalex*0+3,scaley*3],[scalex*1,scaley*3]];
+	spriteMapIndex.set("pacmanUp",pD);
+	spriteMapIndex.set("pacmanLeft",1);
+	spriteMapIndex.set("pacmanRight",2);
+	spriteMapIndex.set("pacmanDown",pD);
+	spriteMapIndex.set("Alien0",4);
+	spriteMapIndex.set("Alien1",5);
+	spriteMapIndex.set("Alien2",6);
+	spriteMapIndex.set("Alien3",7);
+
 }
 
 function startGame()
@@ -84,6 +102,18 @@ function updateGameArea() {
 function setField()
 {
 	// draw here the objects from spritesheet
+	drawSprite("pacmanDown",pacmanX*scalex,pacmanY*scaley);
+}
+
+function drawSprite(spritename , coordinateX , coordinateY)
+{
+	var spr ;
+	if( spriteMapIndex.get(spritename) != undefined)
+	{
+		spr = spriteMapIndex.get(spritename);
+		myContext.drawImage(spritesheet,spr[0][0],spr[0][1],scalex
+			,scaley,coordinateX,coordinateY,scalex,scaley);
+	}
 }
 
 function drawRect(x, y, width, height, style) {
@@ -99,6 +129,6 @@ function drawMaze() {
 		for(var j = 0 ; j < grid[i].length ; ++j)
 			if(grid[i][j] == 1) {
 				console.log('here');
-				drawRect(i*scalex, j*scaley, grid[i][j]*scalex, grid[i][j]*scaley, 'blue');
+				drawRect(i*scalex, j*scaley, grid[i][j]*scalex, grid[i][j]*scaley, 'black');
 			}
 }
